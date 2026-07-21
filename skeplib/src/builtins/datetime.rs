b88b58d1c70d5953238fab9d6593c1878ccd27f3
AayushMainali-Github <aayushmainali.codes@gmail.com>
2026-07-21 16:19:12 +0000

    fix(ir): align BuiltinSig tables with Result/Vec shapes
    Sema already typed datetime.parseUnix, fs.* IO helpers, and os.exec*
    as Result / Vec[String], but static BuiltinSig metadata lagged behind
    so IR verify rejected programs that passed check.

--- skeplib/src/builtins/datetime.rs
+++ skeplib/src/builtins/datetime.rs
@@ -1,3 +1,5 @@
+use std::sync::LazyLock;
+
 use crate::types::TypeInfo;
 
 use super::{BuiltinKind, BuiltinSig};
@@ -6,82 +8,91 @@ const DATETIME_NOW_PARAMS: &[TypeInfo] = &[];
 const DATETIME_UNIX_PARAMS: &[TypeInfo] = &[TypeInfo::Int];
 const DATETIME_PARSE_PARAMS: &[TypeInfo] = &[TypeInfo::String];
 
-pub(super) const SIGS: &[BuiltinSig] = &[
-    BuiltinSig {
-        package: "datetime",
-        name: "nowUnix",
-        params: DATETIME_NOW_PARAMS,
-        ret: TypeInfo::Int,
-        kind: BuiltinKind::FixedArity,
-    },
-    BuiltinSig {
-        package: "datetime",
-        name: "nowMillis",
-        params: DATETIME_NOW_PARAMS,
-        ret: TypeInfo::Int,
-        kind: BuiltinKind::FixedArity,
-    },
-    BuiltinSig {
-        package: "datetime",
-        name: "fromUnix",
-        params: DATETIME_UNIX_PARAMS,
-        ret: TypeInfo::String,
-        kind: BuiltinKind::FixedArity,
-    },
-    BuiltinSig {
-        package: "datetime",
-        name: "fromMillis",
-        params: DATETIME_UNIX_PARAMS,
-        ret: TypeInfo::String,
-        kind: BuiltinKind::FixedArity,
-    },
-    BuiltinSig {
-        package: "datetime",
-        name: "parseUnix",
-        params: DATETIME_PARSE_PARAMS,
-        ret: TypeInfo::Int,
-        kind: BuiltinKind::FixedArity,
-    },
-    BuiltinSig {
-        package: "datetime",
-        name: "year",
-        params: DATETIME_UNIX_PARAMS,
-        ret: TypeInfo::Int,
-        kind: BuiltinKind::FixedArity,
-    },
-    BuiltinSig {
-        package: "datetime",
-        name: "month",
-        params: DATETIME_UNIX_PARAMS,
-        ret: TypeInfo::Int,
-        kind: BuiltinKind::FixedArity,
-    },
-    BuiltinSig {
-        package: "datetime",
-        name: "day",
-        params: DATETIME_UNIX_PARAMS,
-        ret: TypeInfo::Int,
-        kind: BuiltinKind::FixedArity,
-    },
-    BuiltinSig {
-        package: "datetime",
-        name: "hour",
-        params: DATETIME_UNIX_PARAMS,
-        ret: TypeInfo::Int,
-        kind: BuiltinKind::FixedArity,
-    },
-    BuiltinSig {
-        package: "datetime",
-        name: "minute",
-        params: DATETIME_UNIX_PARAMS,
-        ret: TypeInfo::Int,
-        kind: BuiltinKind::FixedArity,
-    },
-    BuiltinSig {
-        package: "datetime",
-        name: "second",
-        params: DATETIME_UNIX_PARAMS,
-        ret: TypeInfo::Int,
-        kind: BuiltinKind::FixedArity,
-    },
-];
+fn result_int_string() -> TypeInfo {
+    TypeInfo::Result {
+        ok: Box::new(TypeInfo::Int),
+        err: Box::new(TypeInfo::String),
+    }
+}
+
+pub(super) static SIGS: LazyLock<Vec<BuiltinSig>> = LazyLock::new(|| {
+    vec![
+        BuiltinSig {
+            package: "datetime",
+            name: "nowUnix",
+            params: DATETIME_NOW_PARAMS,
+            ret: TypeInfo::Int,
+            kind: BuiltinKind::FixedArity,
+        },
+        BuiltinSig {
+            package: "datetime",
+            name: "nowMillis",
+            params: DATETIME_NOW_PARAMS,
+            ret: TypeInfo::Int,
+            kind: BuiltinKind::FixedArity,
+        },
+        BuiltinSig {
+            package: "datetime",
+            name: "fromUnix",
+            params: DATETIME_UNIX_PARAMS,
+            ret: TypeInfo::String,
+            kind: BuiltinKind::FixedArity,
+        },
+        BuiltinSig {
+            package: "datetime",
+            name: "fromMillis",
+            params: DATETIME_UNIX_PARAMS,
+            ret: TypeInfo::String,
+            kind: BuiltinKind::FixedArity,
+        },
+        BuiltinSig {
+            package: "datetime",
+            name: "parseUnix",
+            params: DATETIME_PARSE_PARAMS,
+            ret: result_int_string(),
+            kind: BuiltinKind::FixedArity,
+        },
+        BuiltinSig {
+            package: "datetime",
+            name: "year",
+            params: DATETIME_UNIX_PARAMS,
+            ret: TypeInfo::Int,
+            kind: BuiltinKind::FixedArity,
+        },
+        BuiltinSig {
+            package: "datetime",
+            name: "month",
+            params: DATETIME_UNIX_PARAMS,
+            ret: TypeInfo::Int,
+            kind: BuiltinKind::FixedArity,
+        },
+        BuiltinSig {
+            package: "datetime",
+            name: "day",
+            params: DATETIME_UNIX_PARAMS,
+            ret: TypeInfo::Int,
+            kind: BuiltinKind::FixedArity,
+        },
+        BuiltinSig {
+            package: "datetime",
+            name: "hour",
+            params: DATETIME_UNIX_PARAMS,
+            ret: TypeInfo::Int,
+            kind: BuiltinKind::FixedArity,
+        },
+        BuiltinSig {
+            package: "datetime",
+            name: "minute",
+            params: DATETIME_UNIX_PARAMS,
+            ret: TypeInfo::Int,
+            kind: BuiltinKind::FixedArity,
+        },
+        BuiltinSig {
+            package: "datetime",
+            name: "second",
+            params: DATETIME_UNIX_PARAMS,
+            ret: TypeInfo::Int,
+            kind: BuiltinKind::FixedArity,
+        },
+    ]
+});
